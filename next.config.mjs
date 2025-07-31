56fc8bac2125dab3b07450868a337cb7063a2e78
yugwhite <yugwhite@outlook.com>
2025-07-31 22:35:23 +0800

    feat: opt

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,6 @@
-import {withSentryConfig} from "@sentry/nextjs";
 import createNextIntlPlugin from "next-intl/plugin";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
-export default withSentryConfig(withNextIntl(nextConfig), {
-// For all available options, see:
-// https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-org: "limgx",
-project: "javascript-solidstart",
-
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-
-// Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-// tunnelRoute: "/monitoring",
-
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-});
+export default withNextIntl(nextConfig)
