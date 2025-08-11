6bf8cfabb9fd130c2feca499aed59c86c46023a5
嘎嘎你呗 <yugwhite@outlook.com>
2025-08-11 23:40:03 +0800

    feat: remove

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import createNextIntlPlugin from "next-intl/plugin";
-import { isLocal } from "./src/lib/utils";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: isLocal() ? "" : "https://static.limgx.com"
 };
 
 const withNextIntl = createNextIntlPlugin();
